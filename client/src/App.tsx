d080d84eea3212a10f9dfb4c7244243d7d7b348c
bailey <149991366+bja-dev@users.noreply.github.com>
2026-09-25 15:16:53 +1200

    feat: began rewrite to support changing metrics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,24 @@
-import BarChart from "./components/BarChart";
-import { mapTeamStanding, type RawTeamSeasonData, type TeamWinRateDataPoint } from "./types";
-import { useEffect, useState } from "react";
-import { getSortedWinRateData } from "./utils/processData";
-
+import { type ChartMetricKey, mapTeamStanding, METRIC_OPTIONS, type RawTeamSeasonData } from "./types";
+import { useState } from "react";
 
 export default function App() {
-  const [data, setData] = useState<TeamWinRateDataPoint[] | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function loadData(path: string) {
-      // TODO: make generic
-      // path is relative to public
-      try {
-        const resp = await fetch(path);
-        const rawData = await resp.json() as Promise<RawTeamSeasonData[]>
-        setData(getSortedWinRateData((await rawData).map(mapTeamStanding)))
-      } catch (error) {
-        console.log(`An error occurred when fetching the json: ${error}`)
-      } finally {
-        setLoading(false)
-      }
-    }
-    loadData('data/2526-team-season-data.json')
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
+  const [metric, setMetric] = useState<ChartMetricKey>("Points")
   return (
     <main className="app-container">
       <h2>English Premier League Stats 2025/2026 Season</h2>
-      <BarChart data={data} />
+      <div id="controls">
+        <h4>controls</h4>
+        <select
+          value={metric}
+          onChange={(e) => setMetric(e.target.value as ChartMetricKey)}
+        >
+          {METRIC_OPTIONS.map((metricKey) => (
+            <option key={metricKey} value={metricKey}>
+              {metricKey}
+            </option>
+          ))}
+        </select>
+      </div>
     </main>
   );
 }
